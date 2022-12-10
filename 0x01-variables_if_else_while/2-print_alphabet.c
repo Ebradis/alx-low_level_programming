6f93445751934961abd67279d73747e26fd22441
ebradis <ebraddis@gmail.com>
2022-12-10 12:00:38 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (succcess)
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		putchar(c);
+	putchar('\n')
+		return (0)
+
+}
