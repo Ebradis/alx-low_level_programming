8bc5dba84776ccd9ca0d15c4611c2381a525171c
ebradis <ebraddis@gmail.com>
2022-12-11 00:04:11 -0800

    print alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; c <= 'z'; c++)
+		putchar(l);
+	for (l = 'A'; c <= 'Z'; c++)
+		putchar(l);
+	putchar('\n');
+	return (0);
+
+}
