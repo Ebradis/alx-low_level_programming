92f2a1c3d98fdbb099a5f8d6015fb116f201ca8e
ebradis <ebraddis@gmail.com>
2022-12-11 00:57:39 -0800

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char r;
+
+	for (r = 'z'; r >= 'z'; r--)
+		putchar(r);
+	putchar('\n');
+	return (0);
+}
