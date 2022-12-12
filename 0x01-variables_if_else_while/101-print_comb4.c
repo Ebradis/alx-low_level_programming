82a5f8912060a8db67b123e1df28dcd09cbe361f
ebradis <ebraddis@gmail.com>
2022-12-12 01:49:54 -0800

    comb of 3 digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - that prints all possible different combinations of three digits
+ * main - Entry point
  *
- * Return : 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
