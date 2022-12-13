cfae021381c26a1c0e5b767606943269bceaf5df
ebradis <ebraddis@gmail.com>
2022-12-13 09:15:40 -0800

    play alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_in_lowercase - Entry point
+ *
+ * Return:0
+ */
+
+void print_alphabet(void)
+
+{
+	char l = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(l);
+		l++;
+	}
+
+	_putchar('\n');
+
+}
